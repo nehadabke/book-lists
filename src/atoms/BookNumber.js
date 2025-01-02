b759a739559f23d3c848609ff9c0d5184f28a3f9
Neha Dabke <ndabke@amazon.com>
2025-01-02 14:48:17 -0500

    Updates

--- src/atoms/BookNumber.js
+++
@@ -1,9 +0,0 @@
-import "../styles/atoms/BookNumberStyles.css";
-
-const BookNumber = ({number}) => {
-    return <div className="bookNumberButton">
-        <p className="bookNumber">{number}</p>
-        </div>;
-  }  
-
-export default BookNumber;
--- src/atoms/Header.js
+++
@@ -1,9 +0,0 @@
-import "../styles/atoms/HeaderStyles.css";
-
-const Header = ({title}) =>  {
-    return (
-        <h1 className="title">{title}</h1>
-    );
-  };
-  
-export default Header;
